0e36f559e744dde52f339a9efe524598d6e42221
dayeon <163419727+dayeon2423004@users.noreply.github.com>
2026-02-21 00:37:16 +0900

    chore: restructure project folders for better modularity (#2)

--- client/protocol.py
+++ client/protocol.py
@@ -1,1 +1,30 @@
-# protocol
+# client/protocol.py
+import json
+
+def make_ready():
+    return json.dumps({
+        "type": "READY"
+    })
+
+def make_chat(msg):
+    return json.dumps({
+        "type": "CHAT",
+        "message": msg
+    })
+
+# 서버 수신 메세지 변환
+def parse_message(message):
+    data = json.loads(message)
+
+    msg_type = data["type"]
+
+    if msg_type == "START":
+        return ("START", data["color"])
+
+    elif msg_type == "MOVE":
+        return ("MOVE", data)
+
+    elif msg_type == "CHAT":
+        return ("CHAT", data["message"])
+
+    return ("UNKNOWN",)
