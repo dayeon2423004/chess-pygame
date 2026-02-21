6fba314286d0c51a30ea280fd8e26f62110779f9
dayeon <dayeon2423004@g.yju.ac.kr>
2026-02-22 00:57:03 +0900

    refactor: simplify game initialization and use protocol message builders

--- server/game_init.py
+++ server/game_init.py
@@ -15,46 +15,19 @@ def handle_join(server_socket, client_addr):
     if client_addr not in client_list:
         client_list.append(client_addr)
 
-        protocol.send(server_socket, client_addr, {
-            "type": "JOIN_OK"
-        })
+        protocol.send(server_socket, client_addr,
+                      protocol.make_join_ok())
         
-    # 2명 모이면 게임 시작
+    # 2명 모이면 시작
     if len(client_list) == 2 and game_status is None:
         print("게임 시작")
 
-        protocol.send(server_socket, client_list[0], {
-            "type": "START",
-            "color": "black"
-        })
+        # black
+        protocol.send(server_socket, client_list[0],
+                      protocol.make_start("black", "white"))
 
-        protocol.send(server_socket, client_list[1], {
-            "type": "START",
-            "color": "white"
-        })
+        # white
+        protocol.send(server_socket, client_list[1],
+                      protocol.make_start("white", "white"))
 
-        game_status = 1
-
-        # white 먼저 턴
-        protocol.send(server_socket, client_list[1], {
-            "type": "TURN"
-        })
-        
-# 턴 관리
-def handle_turn(server_socket):
-    global game_status
-
-    if len(client_list) != 2:
-        return
-
-    game_status += 1
-
-    # 짝수면 black
-    if game_status % 2 == 0:
-        protocol.send(server_socket, client_list[0], {
-            "type": "TURN"
-        })
-    else:
-        protocol.send(server_socket, client_list[1], {
-            "type": "TURN"
-        })
+        game_status = "PLAYING"
