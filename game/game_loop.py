1733c99dc3033b97c85c20a6ce6289dad0403f0a
dayeon <dayeon2423004@g.yju.ac.kr>
2026-02-23 01:18:59 +0900

    feat: implement network message processing and board state updates

--- game/game_loop.py
+++ game/game_loop.py
@@ -1,7 +1,31 @@
 # game/game_loop.py
 from .input_handler import handle_events
 from .board_render import render
+from client.network import recv_queue
+from client import protocol
 import pygame
+
+
+# 상태 업데이트
+def update_board(state, data):
+    from_x, from_y = data["from"]
+    to_x, to_y = data["to"]
+
+    board = state["piece"]
+
+    piece = board[from_x][from_y]
+    board[from_x][from_y] = None
+    board[to_x][to_y] = piece
+
+    # 턴 관리
+    state["turn"] = data["next_turn"]
+
+    # 드래그 상태 초기화
+    state["selected_piece"] = None
+    state["dragging"] = False
+    state["drag_pos"] = None
+
+    print("서버의 메세지", data)
 
 # 게임 루프
 def run(state):
@@ -9,9 +33,25 @@ def run(state):
     running = True
 
     while running:
+
+        # 네트워크 메시지 처리
+        while not recv_queue.empty():
+            message = recv_queue.get()
+            result = protocol.parse_message(message)
+
+            if result[0] == "MOVE":
+                data = result[1]
+                if data["success"]:
+                    update_board(state, data)
+
+            elif result[0] == "MOVE_INVALID":
+                print("잘못된 이동입니다.")
+
+        # 이벤트 처리
         running = handle_events(state)
 
+        # 렌더링
         render(state)
-
         pygame.display.flip()
+
         clock.tick(60)
