774076501d59f976ff53f7e6be30e4ab2757a3dc
dayeon <dayeon2423004@g.yju.ac.kr>
2026-02-22 16:46:53 +0900

    feat: add king movement and basic safety check

--- chess_logic/pieces/king.py
+++ chess_logic/pieces/king.py
@@ -1,1 +1,7 @@
-# king.py
+# chess_logic/pieces/queen.py
+
+def is_valid_king_move(from_x, from_y, to_x, to_y):
+    dx = abs(to_x - from_x)
+    dy = abs(to_y - from_y)
+
+    return dx <= 1 and dy <= 1
